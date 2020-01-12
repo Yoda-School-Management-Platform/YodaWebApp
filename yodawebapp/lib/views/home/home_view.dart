97096588132535c983812e6a532602259219fc7c
iLePix <31792282+iLePix@users.noreply.github.com>
2020-01-12 13:45:50 +0100

    Changed from (just) Dart to Flutter (and Dart)

---
+++ yodawebapp/lib/views/home/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:yodawebapp/widgets/navigation_bar/navigation_bar.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          NavigationBar()
+        ],
+      ),
+    );
+  }
+
+}
