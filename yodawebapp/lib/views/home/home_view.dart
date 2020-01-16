05930c95838673dc9d28ce4a00388d6485f14d80
iLePix <31792282+iLePix@users.noreply.github.com>
2020-01-16 23:12:26 +0100

    A lot

--- yodawebapp/lib/views/home/home_view.dart
+++ yodawebapp/lib/views/home/home_view.dart
@@ -1,19 +1,169 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import 'package:yodawebapp/utils/responsive_layout.dart';
 import 'package:yodawebapp/widgets/navigation_bar/navigation_bar.dart';
+import 'package:yodawebapp/widgets/search/search.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color(0xFFF8FBFF),
+        Color(0xFFFCFDFD),
+      ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[NavBar(), Body()],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveLayout(
+      largeScreen: LargeChild(),
+      smallScreen: SmallChild(),
+    );
+  }
+}
+
+class LargeChild extends StatefulWidget {
+  _LargeChild createState() => _LargeChild();
+}
+
+class _LargeChild extends State<LargeChild>  with SingleTickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 1080,
+      child: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
-          NavigationBar()
+          Container(
+              color: Colors.red,
+              child: Stack(
+                children: <Widget>[
+                  Image.asset('images/background.png'),
+                  Center(
+                    child: SizedBox(
+                      height: 700,
+                      width: 700,
+                      child: Image.asset('images/fluid 5.png'),
+                    ),
+                  )
+                ],
+              )),
+          FractionallySizedBox(
+            alignment: Alignment.centerLeft,
+            widthFactor: .6,
+            child: Padding(
+              padding: EdgeInsets.only(left: 48),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Hello!",
+                      style: TextStyle(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  RichText(
+                    text: TextSpan(
+                        text: "WellCome To ",
+                        style: TextStyle(
+                            fontSize: 80,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                        children: [
+                          TextSpan(
+                              text: "YODA",
+                              style: TextStyle(
+                                  fontSize: 80,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white))
+                        ]),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Search()
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
   }
+}
 
+class SmallChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Hello!",
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Color(0xFF8591B0),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Montserrat-Regular"),
+            ),
+            RichText(
+              text: TextSpan(
+                text: 'WellCome To ',
+                style: TextStyle(fontSize: 40, color: Color(0xFF8591B0)),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Britu',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: Colors.black87)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, top: 20),
+              child: Text("LET’S EXPLORE THE WORLD"),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Image.network(
+                "assets/image_01.png",
+                scale: 1,
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Search(),
+            SizedBox(
+              height: 30,
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
--- yodawebapp/lib/widgets/navigation_bar/navigation_bar.dart
+++ yodawebapp/lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,38 +1,107 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:yodawebapp/utils/responsive_layout.dart';
 
-class NavigationBar extends StatelessWidget {
-  const NavigationBar({Key key}) : super(key: key);
+class NavBar extends StatelessWidget {
+  final navLinks = ["Home", "Products", "Features", "Contact"];
 
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      child: Row(
-        children: <Widget>[
-          SizedBox(height: 80, width: 150, child: Image.asset('assets/images/logo.png'),),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              _NavBarItem('Episodes'),
-              SizedBox(width: 60,),
-              _NavBarItem('About'),
-            ],
-          )
-        ],
-      ),
-    );
+  List<Widget> navItem() {
+    return navLinks.map((text) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Text(
+          text,
+          style: TextStyle(
+              fontFamily: "Montserrat-Bold",
+              fontSize: 15.0,
+              color: Color.fromRGBO(251, 209, 128, 1)),
+        ),
+      );
+    }).toList();
   }
 
-}
-
-
-class _NavBarItem extends StatelessWidget {
-  final String title;
-  const _NavBarItem(this.title);
-
   @override
   Widget build(BuildContext context) {
-    return Text(title, style: TextStyle(fontSize: 18));
+    return Material(
+        color: Color.fromRGBO(40, 40, 40, 1),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 45, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFF2C139),
+                              Color(0xFFCE768D),
+                              Color(0xFF7932CD),
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                    child: Center(
+                      child: Text("Y",
+                          style: TextStyle(fontSize: 30, color: Colors.white)),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Text("YODA",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                          fontSize: 26,
+                          color: Color.fromRGBO(251, 209, 128, 1)))
+                ],
+              ),
+              !ResponsiveLayout.isSmallScreen(context)
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                          Row(
+                            children: navItem(),
+                          ),
+                          Container(
+                              margin: EdgeInsets.only(left: 20),
+                              width: 120,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xFFF2C139),
+                                      Color(0xFFCE768D),
+                                      Color(0xFF7932CD),
+                                    ],
+                                    begin: Alignment.topRight,
+                                    end: Alignment.bottomLeft),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Material(
+                                    color: Colors.transparent,
+                                    child: InkWell(
+                                      onTap: () {},
+                                      child: Center(
+                                        child: Text("Login",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 18,
+                                                letterSpacing: 1,
+                                                fontFamily: "Montserrat-Bold")),
+                                      ),
+                                    )),
+                              )),
+                        ])
+                  : Image.network("assets/images/menu.png",
+                      width: 26, height: 26),
+            ],
+          ),
+        ));
   }
 }
